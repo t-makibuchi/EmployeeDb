e4dd08bbd7f80d063dbfeda1f4ddec5e8fc2cf08
tatsuya makibuchi <t.makibuchi@gmail.com>
2018-07-02 23:41:13 +0900

    add a login page.

---
+++ EmployeeDB/src/main/java/com/employeedb/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.employeedb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.employeedb.entity.Employee;
+import com.employeedb.form.EmployeeForm;
+import com.employeedb.service.EmployeeService;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@Autowired
+	public EmployeeService employeeService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String index() {
+		
+		return "login";
+	}
+	
+	@RequestMapping(value = "/submit", method = RequestMethod.POST)
+	public String submit(EmployeeForm employeeForm) {
+
+		employeeService.create(employeeForm);
+		return "redirect:../employeeList";
+	}
+	
+}
