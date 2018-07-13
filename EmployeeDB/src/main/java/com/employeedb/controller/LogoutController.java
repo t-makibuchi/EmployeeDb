28f794b8005b1e4c8c45a5dd7ab96d57f7ca3671
tatsuya makibuchi <t.makibuchi@gmail.com>
2018-07-13 23:55:50 +0900

    make the LogoutController

---
+++ EmployeeDB/src/main/java/com/employeedb/controller/LogoutController.java
@@ -0,0 +1,30 @@
+package com.employeedb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.employeedb.entity.Employee;
+import com.employeedb.form.EmployeeForm;
+import com.employeedb.service.EmployeeService;
+
+@Controller
+@RequestMapping("")
+public class LogoutController {
+	
+	@Autowired
+	public EmployeeService employeeService;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("iserror",false);
+		return "login";
+	}
+	
+}
