be2c691b283a9c6756d3ad990a04a92491d8e2e5
tatsuya makibuchi <t.makibuchi@gmail.com>
2018-07-24 23:29:03 +0900

    create a CipherManager

--- EmployeeDB/src/main/java/com/employeedb/security/CipherManager.java
+++ EmployeeDB/src/main/java/com/employeedb/security/CipherManager.java
@@ -1,5 +1,29 @@
 package com.employeedb.security;
 
-public class CipherManager {
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CipherManager {
+	private static final String KEY = "6Uy89lkjj3i5Las3";
+    private static final String ALGORITHM = "AES";
+	
+	
+	public static String encrypt(String source) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(KEY.getBytes(), ALGORITHM));
+        return new String(Base64.getEncoder().encode(cipher.doFinal(source.getBytes())));
+    }
+    
+    public static String decrypt(String encryptSource) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(KEY.getBytes(), ALGORITHM));
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptSource.getBytes())));
+    }
 }
--- EmployeeDB/src/main/java/com/employeedb/security/CipherProvider.java
+++
@@ -1,5 +0,0 @@
-package com.employeedb.security;
-
-public class CipherProvider {
-
-}
