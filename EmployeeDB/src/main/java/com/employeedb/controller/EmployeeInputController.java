cd01da46ab374043a427399f8f487d3eb0b7a013
tatsuya makibuchi <t.makibuchi@gmail.com>
2018-08-12 23:59:31 +0900

    create validations

--- EmployeeDB/src/main/java/com/employeedb/controller/EmployeeInputController.java
+++ EmployeeDB/src/main/java/com/employeedb/controller/EmployeeInputController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.employeedb.entity.Employee;
 import com.employeedb.form.EmployeeInputForm;
 import com.employeedb.service.EmployeeService;
 
