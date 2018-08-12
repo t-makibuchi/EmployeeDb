470c15fe35d3bee1365ee16c66cca4cf4495de5e
tatsuya makibuchi <t.makibuchi@gmail.com>
2018-08-13 00:02:54 +0900

    create validations.

--- EmployeeDB/src/main/java/com/employeedb/controller/EmployeeInputController.java
+++ EmployeeDB/src/main/java/com/employeedb/controller/EmployeeInputController.java
@@ -1,7 +1,5 @@
 package com.employeedb.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,7 +7,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.employeedb.form.EmployeeInputForm;
